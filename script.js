c856a8b6931c408176c4de1cd43e180b298fa22c
Tyler <tyler.le@edu.sait.ca>
2023-06-14 13:44:54 -0600

    Creates vector field

--- script.js
+++ script.js
@@ -14,11 +14,35 @@ class Particle {
         this.effect = effect;
         this.x = Math.floor(Math.random() * this.effect.width);
         this.y = Math.floor(Math.random() * this.effect.height);
-        this.speedX = 1;
-        this.speedY = 1;
+        this.speedX = Math.random() * 5 - 2.5;
+        this.speedY = Math.random() * 5 - 2.5;
+        this.history = [{x: this.x, y: this.y}]
+        this.maxLength = Math.floor(Math.random() * 200 + 10);
+        this.angle = 0
     }
     draw(context){
-        context.fillRect(this.x, this.y, 10,10);
+        context.beginPath();
+        context.moveTo(this.history[0].x, this.history[0].y);
+        for (let i = 0; i < this.history.length; i++){
+            context.lineTo(this.history[i].x, this.history[i].y);
+        }
+        context.stroke();
+    }
+    update(){
+        let x = Math.floor(this.x / this.effect.cellSize);
+        let y = Math.floor(this.y / this.effect.cellSize);
+        let index = y * this.effect.cols + x;
+        this.angle = this.effect.flowField[index];
+
+        this.speedX = Math.cos(this.angle);
+        this.speedy = Math.sin(this.angle);
+        this.x += this.speedX;
+        this.y += this.speedY;
+
+        this.history.push({x: this.x, y: this.y})
+        if ( this.history.length > this.maxLength){
+            this.history.shift();
+        }
     }
 }
 
@@ -27,10 +51,27 @@ class Effect {
         this.width = width;
         this.height = height;
         this.particles = [];
-        this.numberOfParticles = 50;
+        this.numberOfParticles = 500;
+        this.cellSize = 20;
+        this.rows;
+        this.cols;
+        this.flowField = [];
+        this.curve = 0.5
+        this.zoom = 0.2
         this.init()
      }
      init(){
+        // creates flow field
+        this.rows = Math.floor(this.height / this.cellSize);
+        this.cols = Math.floor(this.width / this.cellSize);
+        this.flowField = [];
+        for (let y = 0; y < this.rows; y++){
+            for (let x = 0; x < this.cols; x++){
+                let angle = (Math.cos(x * this.zoom) + Math.sin(y * this.zoom) * this.curve);
+                this.flowField.push(angle);
+            }
+        }
+
         // create particles
         for (let i = 0; i < this.numberOfParticles; i++){
             this.particles.push(new Particle(this))
@@ -40,10 +81,18 @@ class Effect {
      render(context){
         this.particles.forEach(particle => {
             particle.draw(context);
+            particle.update();
         });
      }
 }
 
 const effect = new Effect(canvas.width, canvas.height);
-effect.render(ctx)
-console.log(effect)
+effect.render(ctx);
+console.log(effect);
+
+function animate(){
+    ctx.clearRect(0,0, canvas.width, canvas.height);
+    effect.render(ctx);
+    requestAnimationFrame(animate);
+}
+animate();
